06f3ac3679f0739116ed2a036d5500b413f9b6d8
nurrebass1 <root@foreman.openstacklocal>
2021-03-05 22:31:08 +0000

    Updated certificate removing in cleanup script

--- cleanup.py
+++ cleanup.py
@@ -3,11 +3,19 @@ import subprocess
 
 def cleanup():
     print('------------- DELETING MANIFESTS ------------')
+    
     subprocess.run('rm -rf /etc/puppetlabs/code/environments/production/manifests/', shell=True)
     print('------------- REMOVING CERTIFICATES ---------')
-    # TODO: fix cert name
-    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean XXXXX', shell=True)
+    
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean agent1.projectx', shell=True)   
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean development1.projectx', shell=True)
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean development21.projectx', shell=True)
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean storage1.projectx', shell=True)
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean storage2.projectx', shell=True)
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean compiler1.projectx', shell=True)
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean compiler2.projectx', shell=True)
     print('<---------- DELETING INSTANCES ------->')
+    
     subprocess.run('nova delete storage1.projectx', shell=True)
     subprocess.run('nova delete storage2.projectx', shell=True)
     subprocess.run('nova delete compiler1.projectx', shell=True)
@@ -15,6 +23,8 @@ def cleanup():
     subprocess.run('nova delete development1.projectx', shell=True)
     subprocess.run('nova delete development2.projectx', shell=True)
     subprocess.run('nova delete host1.agent', shell=True)
+    
+    #TODO: remove from Foreman
     print('-------------- DONE ------------')
 
 
