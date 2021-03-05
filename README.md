008c1a1c7c0c092901ea42ae683f652862d91d2d
root <root@foreman.openstacklocal>
2021-03-05 17:26:23 +0000

    Added cleanup.py and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# io_19
+# A development environment by IO19
+
+
+## Prequisites
+In order to deploy the development invirontments XX is required.
+
+Navigate to root by running 
+
+```
+sudo su
+cd
+```
+## Clone project
+```
+git clone 
+```
+
+## How to run
+```
+cd io19
+
+python3 deployer.py
+```
+
+## Cleanup
+To cleanup the environment run
+
+```
+python3 cleanup.py
+```
+
+
+
 
---
+++ cleanup.py
@@ -0,0 +1,22 @@
+import subprocess
+
+
+def cleanup():
+    print('------------- DELETING MANIFESTS ------------')
+    subprocess.run('rm -rf /etc/puppetlabs/code/environments/production/manifests/', shell=True)
+    print('------------- REMOVING CERTIFICATES ---------')
+    # TODO: fix cert name
+    subprocess.run('/opt/puppetlabs/puppet/bin/puppet cert clean XXXXXX', shell=True)
+    print('<---------- DELETING INSTANCES ------->')
+    subprocess.run('nova delete storage1.projectx', shell=True)
+    subprocess.run('nova delete storage2.projectx', shell=True)
+    subprocess.run('nova delete compiler1.projectx', shell=True)
+    subprocess.run('nova delete compiler2.projectx', shell=True)
+    subprocess.run('nova delete development1.projectx', shell=True)
+    subprocess.run('nova delete development2.projectx', shell=True)
+    subprocess.run('nova delete host1.agent', shell=True)
+    print('-------------- DONE ------------')
+
+
+cleanup()
+
