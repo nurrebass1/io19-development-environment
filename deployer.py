5e942c9dcb098e48b42a0783785727acfeb69fdc
nurrebass1 <s351643@oslomet.no>
2021-03-06 13:48:37 +0100

    Testing

--- deployer.py
+++ deployer.py
@@ -41,6 +41,8 @@ def deploy():
     subprocess.run('cp default_hostgroup.yaml /etc/foreman/plugins/', shell=True)
 
     for folder in folders:
+        print('###############################################')
+        print('cp -r ' + folder + '/' + ' /etc/puppetlabs/code/environments/production/manifests/')
         subprocess.run('cp -r ' + folder + '/' + ' /etc/puppetlabs/code/environments/production/manifests/', shell=True)
 
     subprocess.run('service foreman restart', shell=True)
